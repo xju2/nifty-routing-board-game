8b9783fce35b8bafe2a536fc6e214d7ad170f304
Xiangyang Ju <xiangyang.ju@gmail.com>
2026-02-22 22:48:34 -0800

    latest version

--- examples/nifty-proto/read.py
+++ examples/nifty-proto/read.py
@@ -8,5 +8,3 @@ with open("nifty_common.bin", "rb") as f:
     converter = NiftyConverter()
     data = converter.to_pyg(batch)
     print("GraphBatch converted to PyTorch Geometric Data object:", data)
-
-# print(batch)
